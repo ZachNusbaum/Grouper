f3276360a99201f6e315d6a037cef600740c7479
Zach Nusbaum <zach@zdnmail.com>
2019-05-28 10:28:52 -0700

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Homepage from "./components/Homepage";
+import Events from "./components/Events";
+import Event from "./components/Event";
+import Attendees from "./components/Attendees";
+import Register from "./components/Register";
+import Login from "./components/Login";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          < Redirect exact path = "/" to = "/Homepage" />
+          < Route path = "/Homepage" component = { Homepage } />
+          < Route path = "/Events" component = { Events } />
+          < Route path = "/Event" component = { Event } />
+          < Route path = "/Attendees" component = { Attendees } />
+          < Route path = "/Register" component = { Register } />
+          < Route path = "/Login" component = { Login } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,39 @@
+﻿import React from "react";
+import styles from "./navbar.module.css";
+
+//TODO Web Template Studio: Add a new link in the NavBar for your page here.
+// A skip link is included as an accesibility best practice. For more information visit https://www.w3.org/WAI/WCAG21/Techniques/general/G1.
+export default function NavBar() {
+  return (
+    <React.Fragment>
+      <div className={styles.skipLink}>
+        <a href="#mainContent">Skip to Main Content</a>
+      </div>
+      <nav className="navbar navbar-expand-sm navbar-light border-bottom justify-content-between">
+        <a className="navbar-brand" href="/">
+          Grouper
+        </a>
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="Homepage">
+            Homepage
+          </a>
+          <a className="nav-item nav-link active" href="Events">
+            Events
+          </a>
+          <a className="nav-item nav-link active" href="Event">
+            Event
+          </a>
+          <a className="nav-item nav-link active" href="Attendees">
+            Attendees
+          </a>
+          <a className="nav-item nav-link active" href="Register">
+            Register
+          </a>
+          <a className="nav-item nav-link active" href="Login">
+            Login
+          </a>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+}
